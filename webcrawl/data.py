ce3b55f0ce272956544e046939f93bea938ae317
umashankar-300 <33493192+umashankar-300@users.noreply.github.com>
2018-02-24 09:38:15 +0530

    Update data.py

--- webcrawl/data.py
+++ webcrawl/data.py
@@ -30,52 +30,18 @@ while(True):
 		description = description + detail.text +"."
 	description = "\"" + description + "\""
 	try:
-		print(soup.find("p",{"class" : "pdp-style-note"}).text)
-		style = "\"" + soup.find("p",{"class" : "pdp-style-note"}).text + "\""
-	except:
-		style="none"
-		pass
-	try:
-		print(soup.find("div",{"class" : "breadcrumbs-container"}).text)
-		breadcrumb = "\"" + soup.find("div",{"class" : "breadcrumbs-container"}).text + "\""
-	except:
-		breadcrumb = "none"
-		pass
-	try:
 		print(soup.find("h1",{"class" : "pdp-title"}).text)
 		title = "\"" + soup.find("h1",{"class" : "pdp-title"}).text + "\""
 	except:
 		title = "none"
 		pass
-	try:
-		print(soup.find("p", {"class" : "pdp-discount-container"}).text)
-		discount = "\"" + soup.find("p", {"class" : "pdp-discount-container"}).text + "\""
-	except:
-		discount = "none"
-		pass
-	try:
-		print(soup.find("strong", {"class" : "pdp-price"}).text)
-		sp = "\"" + soup.find("strong", {"class" : "pdp-price"}).text + "\""
-	except:
-		sp = "none"
-		pass
-	#imgdiv = soup.find("div", {"class" : "thumbnails-vertical-container"})
-	#print(imgdiv)
+
+	
 	i=0
 	directory = "./"+str(dircount)
 	if not os.path.exists(directory):
 	    os.makedirs(directory)
-	#for img in imgdiv.findAll('button'):
-	#	src = img.img['src']
-	#	src = src.replace("h_68,q_100,w_52", "h_1440,q_100,w_1080")
-	#	response = requests.get(src, stream = True)
-	#	imgname=directory+"/"+"img"+str(i)+".jpg"
-	#	with open(imgname, 'wb') as out_file:
-	#		shutil.copyfileobj(response.raw, out_file)
-	#	del response
-	#	print(src)
-	#	i = i + 1
-	#browser.quit()
+	
 	
 	csvfile.write(title + "," + style + "," + description + "," + breadcrumb + "," + discount + "," + sp + "," + "\n")
 
