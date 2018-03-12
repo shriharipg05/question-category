3409eebfa66ec78aefe1a8c210bc845e2d3c4049
shriharipg05 <shriharipg05@gmail.com>
2018-03-12 19:48:49 +0530

    Add files via upload

--- test.py
+++ test.py
@@ -1,206 +1,240 @@
-import html
-import pprint
-import re
-from html.parser import HTMLParser
+import re, os
 
-from sklearn.cross_validation import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics import confusion_matrix
-from sklearn.svm import SVC
-
-
-class ReutersParser(HTMLParser):
-    """
-    ReutersParser subclasses HTMLParser and is used to open the SGML
-    files associated with the Reuters-21578 categorised test collection.
-
-    The parser is a generator and will yield a single document at a time.
-    Since the data will be chunked on parsing, it is necessary to keep 
-    some internal state of when tags have been "entered" and "exited".
-    Hence the in_body, in_topics and in_topic_d boolean members.
-    """
-    def __init__(self, encoding='latin-1'):
-        """
-        Initialise the superclass (HTMLParser) and reset the parser.
-        Sets the encoding of the SGML files by default to latin-1.
-        """
-        html.parser.HTMLParser.__init__(self)
-        self._reset()
-        self.encoding = encoding
-
-    def _reset(self):
-        """
-        This is called only on initialisation of the parser class
-        and when a new topic-body tuple has been generated. It
-        resets all off the state so that a new tuple can be subsequently
-        generated.
-        """
-        self.in_body = False
-        self.in_topics = False
-        self.in_topic_d = False
-        self.body = ""
-        self.topics = []
-        self.topic_d = ""
-
-    def parse(self, fd):
-        """
-        parse accepts a file descriptor and loads the data in chunks
-        in order to minimise memory usage. It then yields new documents
-        as they are parsed.
-        """
-        self.docs = []
-        for chunk in fd:
-            self.feed(chunk.decode(self.encoding))
-            for doc in self.docs:
-                yield doc
-            self.docs = []
-        self.close()
-
-    def handle_starttag(self, tag, attrs):
-        """
-        This method is used to determine what to do when the parser
-        comes across a particular tag of type "tag". In this instance
-        we simply set the internal state booleans to True if that particular
-        tag has been found.
-        """
-        if tag == "reuters":
-            pass
-        elif tag == "body":
-            self.in_body = True
-        elif tag == "topics":
-            self.in_topics = True
-        elif tag == "d":
-            self.in_topic_d = True 
-
-    def handle_endtag(self, tag):
-        """
-        This method is used to determine what to do when the parser
-        finishes with a particular tag of type "tag". 
-
-        If the tag is a  tag, then we remove all 
-        white-space with a regular expression and then append the 
-        topic-body tuple.
-
-        If the tag is a  or  tag then we simply set
-        the internal state to False for these booleans, respectively.
-
-        If the tag is a  tag (found within a  tag), then we
-        append the particular topic to the "topics" list and 
-        finally reset it.
-        """
-        if tag == "reuters":
-            self.body = re.sub(r'\s+', r' ', self.body)
-            self.docs.append( (self.topics, self.body) )
-            self._reset()
-        elif tag == "body":
-            self.in_body = False
-        elif tag == "topics":
-            self.in_topics = False
-        elif tag == "d":
-            self.in_topic_d = False
-            self.topics.append(self.topic_d)
-            self.topic_d = ""  
-
-    def handle_data(self, data):
-        """
-        The data is simply appended to the appropriate member state
-        for that particular tag, up until the end closing tag appears.
-        """
-        if self.in_body:
-            self.body += data
-        elif self.in_topic_d:
-            self.topic_d += data
-
-
-def obtain_topic_tags():
-    """
-    Open the topic list file and import all of the topic names
-    taking care to strip the trailing "\n" from each word.
-    """
-    topics = open(
-        "data/all-topics-strings.lc.txt", "r"
-    ).readlines()
-    topics = [t.strip() for t in topics]
-    return topics
-
-def filter_doc_list_through_topics(topics, docs):
-    """
-    Reads all of the documents and creates a new list of two-tuples
-    that contain a single feature entry and the body text, instead of
-    a list of topics. It removes all geographic features and only 
-    retains those documents which have at least one non-geographic
-    topic.
-    """
-    ref_docs = []
-    for d in docs:
-        if d[0] == [] or d[0] == "":
-            continue
-        for t in d[0]:
-            if t in topics:
-                d_tup = (t, d[1])
-                ref_docs.append(d_tup)
-                break
-    return ref_docs
-
-def create_tfidf_training_data(docs):
-    """
-    Creates a document corpus list (by stripping out the
-    class labels), then applies the TF-IDF transform to this
-    list. 
-
-    The function returns both the class label vector (y) and 
-    the corpus token/feature matrix (X).
-    """
-    # Create the training data class labels
-    y = [d[0] for d in docs]
+class BagOfWords(object):
+    """ Implementing a bag of words, words corresponding with their 
+        frequency of usages in a "document" for usage by the Document 
+        class, DocumentClass class and the Pool class."""
     
-    # Create the document corpus list
-    corpus = [d[1] for d in docs]
-
-    # Create the TF-IDF vectoriser and transform the corpus
-    vectorizer = TfidfVectorizer(min_df=1)
-    X = vectorizer.fit_transform(corpus)
-    return X, y
-
-def train_svm(X, y):
-    """
-    Create and train the Support Vector Machine.
-    """
-    svm = SVC(C=1000000.0, gamma=0.0, kernel='rbf')
-    svm.fit(X, y)
-    return svm
-
-
-if __name__ == "__main__":
-    # Create the list of Reuters data and create the parser
-    files = ["data/reut2-%03d.sgm" % r for r in range(0, 22)]
-    parser = ReutersParser()
-
-    # Parse the document and force all generated docs into
-    # a list so that it can be printed out to the console
-    docs = []
-    for fn in files:
-        for d in parser.parse(open(fn, 'rb')):
-            docs.append(d)
-
-    # Obtain the topic tags and filter docs through it 
-    topics = obtain_topic_tags()
-    ref_docs = filter_doc_list_through_topics(topics, docs)
+    def __init__(self):
+        self.__number_of_words = 0
+        self.__bag_of_words = {}
+        
+    def __add__(self, other):
+        """ Overloading of the "+" operator to join two BagOfWords """
+        erg = BagOfWords()
+        sum = erg.__bag_of_words
+        for key in self.__bag_of_words:
+            sum[key] = self.__bag_of_words[key]
+            if key in other.__bag_of_words:
+                sum[key] += other.__bag_of_words[key]
+        for key in other.__bag_of_words:
+            if key not in sum:
+                sum[key] = other.__bag_of_words[key]
+        return erg
+        
+    def add_word(self, word):
+        """ A word is added in the dictionary __bag_of_words"""
+        self.__number_of_words += 1
+        if word in self.__bag_of_words:
+            self.__bag_of_words[word] += 1
+        else:
+            self.__bag_of_words[word] = 1
     
-    # Vectorise and TF-IDF transform the corpus 
-    X, y = create_tfidf_training_data(ref_docs)
+    def len(self):
+        """ Returning the number of different words of an object """
+        return len(self.__bag_of_words)
+    
+    def Words(self):
+        """ Returning a list of the words contained in the object """
+        return self.__bag_of_words.keys()
+    
+        
+    def BagOfWords(self):
+        """ Returning the dictionary, containing the words (keys) with their 
+            frequency (values)"""
+        return self.__bag_of_words
+        
+    def WordFreq(self,word):
+        """ Returning the frequency of a word """
+        if word in self.__bag_of_words:
+            return self.__bag_of_words[word]
+        else:
+            return 0
 
-    # Create the training-test split of the data
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+class Document(object):
+    """ Used both for learning (training) documents and for testing documents. 
+        The optional parameter learn has to be set to True, if a classificator 
+        should be trained. If it is a test document learn has to be set to 
+        False. """
+ 
+    def __init__(self, vocabulary):
+        self.__name = ""
+        self.__document_class = None
+        self._words_and_freq = BagOfWords()
+        Document._vocabulary = vocabulary
+    
+    def read_document(self,filename, learn=False):
+        """ A document is read. It is assumed, that the document is either 
+            encoded in utf-8 or in iso-8859... (latin-1).
+            The words of the document are stored in a Bag of Words, i.e.         
+            self._words_and_freq = BagOfWords() """
+        try:
+            text = open(filename,"r", encoding='utf-8').read()
+        except UnicodeDecodeError:
+            text = open(filename,"r", encoding='latin-1').read()
+        text = text.lower()
+        words = re.split(r"\W", text)
 
-    # Create and train the Support Vector Machine
-    svm = train_svm(X_train, y_train)
+        self._number_of_words = 0
+        for word in words:
+            self._words_and_freq.add_word(word)
+            if learn:
+                Document._vocabulary.add_word(word)
 
-    # Make an array of predictions on the test set
-    pred = svm.predict(X_test)
 
-    # Output the hit-rate and the confusion matrix for each model
-    print(svm.score(X_test, y_test))
-    print(confusion_matrix(pred, y_test))
+    def __add__(self, other):
+        """ Overloading the "+" operator. Adding two documents consists 
+            in adding the BagOfWords of the Documents """
+        res = Document(Document._vocabulary)
+        res._words_and_freq = self._words_and_freq + other._words_and_freq    
+        return res
+    
+    def vocabulary_length(self):
+        """ Returning the length of the vocabulary """
+        return len(Document._vocabulary)
+                
+    def WordsAndFreq(self):
+        """ Returning the dictionary, containing the words (keys) with 
+        their frequency (values) as contained in the BagOfWords attribute 
+        of the document"""
+        return self._words_and_freq.BagOfWords()
+        
+    def Words(self):
+        """ Returning the words of the Document object """
+        d =  self._words_and_freq.BagOfWords()
+        return d.keys()
+    
+    def WordFreq(self,word):
+        """ Returning the number of times the word "word" appeared in the 
+        document """
+        bow =  self._words_and_freq.BagOfWords()
+        if word in bow:
+            return bow[word]
+        else:
+            return 0
+                
+    def __and__(self, other):
+        """ Intersection of two documents. A list of words occuring in 
+        both documents is returned """
+        intersection = []
+        words1 = self.Words()
+        for word in other.Words():
+            if word in words1:
+                intersection += [word]
+        return intersection
+
+
+class DocumentClass(Document):
+    def __init__(self, vocabulary):
+        Document.__init__(self, vocabulary)
+        self._number_of_docs = 0
+
+    def Probability(self,word):
+        """ returns the probabilty of the word "word" given the class "self" """
+        voc_len = Document._vocabulary.len()
+        SumN = 0
+        for i in range(voc_len):
+            SumN = DocumentClass._vocabulary.WordFreq(word)
+        N = self._words_and_freq.WordFreq(word)
+        erg = 1 + N
+        erg /= voc_len + SumN
+        return erg
+
+    def __add__(self,other):
+        """ Overloading the "+" operator. Adding two DocumentClass objects 
+        consists in adding the BagOfWords of the DocumentClass objectss """
+        res = DocumentClass(self._vocabulary)
+        res._words_and_freq = self._words_and_freq + other._words_and_freq 
+ 
+        return res
+
+    def SetNumberOfDocs(self, number):
+        self._number_of_docs = number
+    
+    def NumberOfDocuments(self):
+        return self._number_of_docs
+
+class Pool(object):
+    def __init__(self):
+        self.__document_classes = {}
+        self.__vocabulary = BagOfWords()
+            
+    def sum_words_in_class(self, dclass):
+        """ The number of times all different words of a dclass appear in a class """
+        sum = 0
+        for word in self.__vocabulary.Words():
+            WaF = self.__document_classes[dclass].WordsAndFreq()
+            if word in WaF:
+                sum +=  WaF[word]
+        return sum
+    
+    def learn(self, directory, dclass_name):
+        """ directory is a path, where the files of the class with the name 
+            dclass_name can be found """
+        x = DocumentClass(self.__vocabulary)
+        dir = os.listdir(directory)
+        for file in dir:
+            d = Document(self.__vocabulary)
+            print(directory + "/" + file)
+            d.read_document(directory + "/" +  file, learn = True)
+            x = x + d
+        self.__document_classes[dclass_name] = x
+        x.SetNumberOfDocs(len(dir))
+
+    
+    def Probability(self, doc, dclass = ""):
+        """Calculates the probability for a class dclass given a document doc"""
+        if dclass:
+            sum_dclass = self.sum_words_in_class(dclass)
+            prob = 0
+        
+            d = Document(self.__vocabulary)
+            d.read_document(doc)
+
+            for j in self.__document_classes:
+                sum_j = self.sum_words_in_class(j)
+                prod = 1
+                for i in d.Words():
+                    wf_dclass = 1 + self.__document_classes[dclass].WordFreq(i)
+                    wf = 1 + self.__document_classes[j].WordFreq(i)
+                    r = wf * sum_dclass / (wf_dclass * sum_j)
+                    prod *= r
+                prob += prod * self.__document_classes[j].NumberOfDocuments() / self.__document_classes[dclass].NumberOfDocuments()
+            if prob != 0:
+                return 1 / prob
+            else:
+                return -1
+        else:
+            prob_list = []
+            for dclass in self.__document_classes:
+                prob = self.Probability(doc, dclass)
+                prob_list.append([dclass,prob])
+            prob_list.sort(key = lambda x: x[1], reverse = True)
+            return prob_list
+
+    def DocumentIntersectionWithClasses(self, doc_name):
+        res = [doc_name]
+        for dc in self.__document_classes:
+            d = Document(self.__vocabulary)
+            d.read_document(doc_name, learn=False)
+            o = self.__document_classes[dc] &  d
+            intersection_ratio = len(o) / len(d.Words())
+            res += (dc, intersection_ratio)
+        return res
+		
+DClasses = ["feedbacks",  "products",  "supports"]
+
+base = "quest_train/"
+p = Pool()
+for i in DClasses:
+    p.learn(base + i, i)
+
+
+
+base = "quest_test/"
+for i in DClasses:
+    dir = os.listdir(base + i)
+    for file in dir:
+        res = p.Probability(base + i + "/" + file)
+        print(i + ": " + file + ": " + str(res))
+
